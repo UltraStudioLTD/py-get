40b3f993f7022f59d90770f0a767e94303b419b0
Luka Mamukashvili <57063715+UltraStudioLTD@users.noreply.github.com>
2021-06-11 20:17:39 +0400

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,3 +1,6 @@
+"""
+CLI-based downloader
+"""
 import os.path
 import sys
 from concurrent.futures import as_completed, ThreadPoolExecutor
@@ -10,7 +13,7 @@ from rich import print as printf
 from rich.progress import *
 
 progress = Progress(
-    TextColumn("<[bold yellow]{task.fields[contenttype]}[/]> [bold blue]{task.fields[filename]}", justify="right"),
+    TextColumn("[{task.fields[responsecode]}] <[bold yellow]{task.fields[contenttype]}[/]> [bold blue]{task.fields[filename]}", justify="right"),
     BarColumn(bar_width=None),
     "[progress.percentage]{task.percentage:>3.1f}%",
     "•",
@@ -36,21 +39,18 @@ signal.signal(signal.SIGINT, handle_sigint)
 
 def copy_url(task_id: TaskID, url: str, path: str) -> None:
     """Copy data from a url to a local file."""
+    
     progress.console.log(f"Requesting {url}")
     response = requests.get(url, stream=True)
-    if response.status_code == requests.codes.ok:
-        progress.update(task_id, total=int(len(response.content)))
-        with open(path, "wb") as dest_file:
-            progress.start_task(task_id)
-            for data in response.iter_content(32768):
-                dest_file.write(data)
-                progress.update(task_id, advance=len(data))
-                if done_event.is_set():
-                    return
-        progress.console.log(f"Downloaded {path}")
-    else:
-        printf(f"[red]ERROR![/]\n[yellow]Status Code[/]: [red]{respone.status.code}[/] caused by [yellow]{url}[/]\n")
-        response.raise_for_status()
+    progress.update(task_id, total=int(len(response.content)))
+    with open(path, "wb") as dest_file:
+        progress.start_task(task_id)
+        for data in response.iter_content(32768):
+            dest_file.write(data)
+            progress.update(task_id, advance=len(data))
+            if done_event.is_set():
+                return
+    progress.console.log(f"Downloaded {path}")
 
 
 def download(urls: Iterable[str], dest_dir: str):
@@ -60,14 +60,22 @@ def download(urls: Iterable[str], dest_dir: str):
         with ThreadPoolExecutor(max_workers=4) as pool:
             for url in urls:
                 filename = url.split("/")[-1]
-                contenttype = requests.get(url).headers['Content-Type'].split(';')[0]
+                response = requests.get(url)
+                contenttype = response.headers['Content-Type'].split(';')[0]
                 if contenttype == "text/plain":
                     filename += '.txt'
                 dest_path = os.path.join(dest_dir, filename)
-                task_id = progress.add_task("download", filename=filename, contenttype=contenttype, start=False)
+                if response.status_code == requests.codes.ok:
+                    responsecode = f"[green]{str(response.status_code)}[/]"
+                else:
+                    responsecode = f"[red]{str(response.status_code)}[/]"
+                task_id = progress.add_task("download", filename=filename, contenttype=contenttype, responsecode=responsecode, start=False)
                 pool.submit(copy_url, task_id, url, dest_path)
+                # printf(f"[red]ERROR![/]\n[yellow]Status Code[/]: [red]{respone.status_code}[/] caused by [yellow]{url}[/]\n")
 
 def print_help_msg():
+    """Prints help message"""
+    
     printf("""CLI-based downloader written in Python
 \tUsage: [bold green]python[/] [blue]downloader[/].[green]py[/] [[italic]-h --help[/]] [yellow]URL[/] [[italic]URL2 URL3 ... etc[/]]
 
